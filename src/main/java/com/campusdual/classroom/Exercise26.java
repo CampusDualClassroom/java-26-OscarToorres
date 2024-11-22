53405d8cf9317e81afb8e43d5d1a918961291da8
Oscar Torres Rodriguez <torresrdriguezoscar@gmail.com>
2024-11-22 13:19:31 +0100

    Ejercicio 26 completado

--- src/main/java/com/campusdual/classroom/Exercise26.java
+++ src/main/java/com/campusdual/classroom/Exercise26.java
@@ -1,8 +1,50 @@
 package com.campusdual.classroom;
 
 
+import java.util.Scanner;
+
 public class Exercise26 {
     public static void main(String[] args) {
 
+        Exercise26 ex = new Exercise26();
+        Contact contact = new Contact();
+        Scanner scanner = new Scanner(System.in);
+
+        boolean seguir = true;
+
+        do {
+            ex.menu();
+
+            int opcion = scanner.nextInt();
+
+            switch (opcion){
+
+                case 1:
+                    contact.callMyNumber();
+                    break;
+                case 2:
+                    contact.callOtherNumber("123456789");
+                    break;
+                case 3:
+                    contact.showContactDetails();
+                    break;
+                case 4:
+                    seguir = false;
+
+            }
+
+
+
+        }while(seguir);
+
+
+    }
+
+    public void menu(){
+        System.out.println("====Menu====");
+        System.out.println("1 - Llamar a si mismo");
+        System.out.println("2 - Llamar a un numero");
+        System.out.println("3 - Mostrar lista de contactos");
+        System.out.println("4 - Salir");
     }
 }
